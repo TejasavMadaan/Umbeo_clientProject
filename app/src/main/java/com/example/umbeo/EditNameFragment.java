392337a27c618eb67a77f2892ee9cbf2c5d9690f
mukulsh98 <sharma.mukul938@gmail.com>
2020-06-15 02:06:31 +0530

    Added the EditName functionality to update username.

--- app/src/main/java/com/example/umbeo/EditNameFragment.java
+++ app/src/main/java/com/example/umbeo/EditNameFragment.java
@@ -1,33 +1,31 @@
 package com.example.umbeo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ProfileFragment extends Fragment {
+public class EditNameFragment extends Fragment {
 
-    ImageView lichi,strawbe,address;
-    TextView editName;
+    Button editName;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v= inflater.inflate(R.layout.activity_person,container , false);
+        View v= inflater.inflate(R.layout.activity_editname,container , false);
 
-        editName= v.findViewById(R.id.editName);
+        editName= (Button) v.findViewById(R.id.button);
         editName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditNameFragment frag= new EditNameFragment();
-                getFragmentManager().beginTransaction().replace(R.id.fragment_container, frag).commit();
+                Toast.makeText(getContext(),"User Name Updated Successfully",Toast.LENGTH_LONG).show();
             }
         });
         return v;
